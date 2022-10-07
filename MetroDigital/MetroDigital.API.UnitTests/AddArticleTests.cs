c22fb434bf7e61cb2e7043260b9f19b3a528a1e2
Mihail Teodorescu <mihail.teodorescu@softelligence.net>
2022-10-07 14:01:05 +0300

    add article tests

---
+++ MetroDigital/MetroDigital.API.UnitTests/AddArticleTests.cs
@@ -0,0 +1,120 @@
+using MediatR;
+using MetroDigital.API.Models;
+using MetroDigital.Application.Features.Basket.Queries.AddArticle;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System.Net.Http.Json;
+
+namespace MetroDigital.API.UnitTests
+{
+    public class AddArticleTests
+    {
+        [Fact]
+        public async Task AddArticleReturnsSuccess()
+        {
+            var senderMock = CreateSender(GetAddArticleSuccessResponse());
+
+            var app = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder => builder.ConfigureServices(services =>
+                {
+                    services.AddScoped((_) => senderMock.Object);
+                }));
+
+            using (app)
+            {
+                var client = app.CreateClient();
+                var result = await client.PostAsJsonAsync("/baskets/1/article-line", new ArticlePostRequest("tomato", 60));
+
+                Assert.NotNull(result);
+                Assert.True(result.IsSuccessStatusCode);
+                VerifySender<AddArticleQueryResponse>(senderMock, Times.Once());
+            }
+        }
+
+        [Fact]
+        public async Task AddArticleReturnsNotFound()
+        {
+            var senderMock = CreateSender(GetAddArticleNotFoundResponse());
+
+            var app = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder => builder.ConfigureServices(services =>
+                {
+                    services.AddScoped((_) => senderMock.Object);
+                }));
+
+            using (app)
+            {
+                var client = app.CreateClient();
+                var result = await client.PostAsJsonAsync("/baskets/1/article-line", new ArticlePostRequest("tomato", 60));
+
+                Assert.NotNull(result);
+                Assert.False(result.IsSuccessStatusCode);
+                Assert.True(result.StatusCode == System.Net.HttpStatusCode.NotFound);
+                VerifySender<AddArticleQueryResponse>(senderMock, Times.Once());
+            }
+        }
+
+        [Fact]
+        public async Task AddArticleReturnsBadRequest()
+        {
+            var senderMock = CreateSender(GetAddArticleBadRequestResponse());
+
+            var app = new WebApplicationFactory<Program>()
+                .WithWebHostBuilder(builder => builder.ConfigureServices(services =>
+                {
+                    services.AddScoped((_) => senderMock.Object);
+                }));
+
+            using (app)
+            {
+                var client = app.CreateClient();
+                var result = await client.PostAsJsonAsync("/baskets/1/article-line", new ArticlePostRequest("tomato", 60));
+
+                Assert.NotNull(result);
+                Assert.False(result.IsSuccessStatusCode);
+                Assert.True(result.StatusCode == System.Net.HttpStatusCode.BadRequest);
+                VerifySender<AddArticleQueryResponse>(senderMock, Times.Once());
+            }
+        }
+
+        private static Mock<ISender> CreateSender<TResponse>(TResponse outputData)
+        {
+            var senderMock = new Mock<ISender>();
+            senderMock.Setup(s => s.Send(It.IsAny<IRequest<TResponse>>(), It.IsAny<CancellationToken>())).ReturnsAsync(outputData);
+            return senderMock;
+        }
+
+        private static void VerifySender<TResponse>(Mock<ISender> senderMock, Times times)
+        {
+            senderMock.Verify(s => s.Send(It.IsAny<IRequest<TResponse>>(), It.IsAny<CancellationToken>()), times);
+        }
+
+        private static AddArticleQueryResponse GetAddArticleSuccessResponse()
+        {
+            return new AddArticleQueryResponse
+            {
+                ArticleItem = new Domain.Entities.Article { ArticleId = 1 },
+                StatusCode = System.Net.HttpStatusCode.OK
+            };
+        }
+
+        private static AddArticleQueryResponse GetAddArticleNotFoundResponse()
+        {
+            return new AddArticleQueryResponse
+            {
+                StatusCode = System.Net.HttpStatusCode.NotFound,
+                ErrorMessage = "Not found"
+            };
+        }
+
+        private static AddArticleQueryResponse GetAddArticleBadRequestResponse()
+        {
+            return new AddArticleQueryResponse
+            {
+                StatusCode = System.Net.HttpStatusCode.BadRequest,
+                ErrorMessage = "Bad Request"
+            };
+        }
+    }
+}
