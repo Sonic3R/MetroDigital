4171bd73ebe5713ad78346b4504ece3fed2b2425
Mihail Teodorescu <mihail.teodorescu@softelligence.net>
2022-10-07 11:08:05 +0300

    add default users in migration

--- MetroDigital/MetroDigital.Infrastructure/Migrations/20221007080733_Add default users.Designer.cs
+++ MetroDigital/MetroDigital.Infrastructure/Migrations/20221007080733_Add default users.Designer.cs
@@ -3,6 +3,7 @@ using MetroDigital.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MetroDigital.Infrastructure.Migrations
 {
     [DbContext(typeof(MetroDigitalDbContext))]
-    partial class MetroDigitalDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221007080733_Add default users")]
+    partial class Adddefaultusers
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MetroDigital/MetroDigital.Infrastructure/Migrations/20221007080733_Add default users.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MetroDigital.Infrastructure.Migrations
+{
+    public partial class Adddefaultusers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Name" },
+                values: new object[] { 1, "Andrei" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Name" },
+                values: new object[] { 2, "Mihail" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "UserId", "Name" },
+                values: new object[] { 3, "Test" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3);
+        }
+    }
+}
